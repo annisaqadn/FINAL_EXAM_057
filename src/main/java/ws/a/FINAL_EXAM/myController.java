03f73f736ab5234304d57a403e5a50beeb540246
Annisaqadn <annisa.qurrota.ft20@mail.umy.ac.id>
2023-01-13 13:56:57 +0700

    Membuat controller

---
+++ src/main/java/ws/a/FINAL_EXAM/myController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ws.a.FINAL_EXAM;
+
+/**
+ *
+ * @author fuadi
+ */
+public class myController {
+    
+}
